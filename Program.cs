c979aeabd8bf485ed96b160997e436acded0fe2d
albertboronov <albertboronov@gmail.com>
2023-04-10 21:17:23 +0700

    half work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main(string[] args){
+string[] daysOfWeek = { "qcs", "qdjjjq", "qd", "qecFFD", "cacsa", "cnKK", "CkaJHHIxab" };
+for (int i = 0; i < daysOfWeek.Length; i++)
+{
+int length = daysOfWeek[i].Length;
+if (length<4)
+{
+    Console.Write($" {daysOfWeek[i]}");
+}
+}}
+Main(args);
